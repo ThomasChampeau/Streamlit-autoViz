9b5bafa12d01a47f5e041d35dfc61010ffdac713
Thomas Champeau <tchampeau.pro@gmail.com>
2026-05-06 09:04:59 +0200

    Update file paths to relative paths for datasets
    Changement du chemin d'accès au BDD. Chemin depuis le rep.

--- graph_stlit.py
+++ graph_stlit.py
@@ -9,27 +9,27 @@ st.title("Maniplulation de données et création de graphiques")
 data_choice = st.selectbox(label= "Quel data set veux tu utiliser ?", options= ["flights.csv","anscombe.csv", "attention.csv","diamonds.csv","car_crashes.csv"])
 
 if data_choice == "flights.csv":
-    data = pd.read_csv(r"C:\Users\KazKami\Documents\BDD exo Seaborn\flights.csv")
+    data = pd.read_csv("flights.csv")
     st.dataframe(data)
     liste_col_x = data.columns.tolist()
     liste_col_y = data.columns.tolist()
 if data_choice == "anscombe.csv":
-    data = pd.read_csv(r"C:\Users\KazKami\Documents\BDD exo Seaborn\anscombe.csv")
+    data = pd.read_csv("anscombe.csv")
     st.dataframe(data)
     liste_col_x = data.columns.tolist()
     liste_col_y = data.columns.tolist()
 if data_choice == "attention.csv":
-    data = pd.read_csv(r"C:\Users\KazKami\Documents\BDD exo Seaborn\attention.csv")
+    data = pd.read_csv("attention.csv")
     st.dataframe(data)
     liste_col_x = data.columns.tolist()
     liste_col_y = data.columns.tolist()
 if data_choice == "diamonds.csv":
-    data = pd.read_csv(r"C:\Users\KazKami\Documents\BDD exo Seaborn\diamonds.csv")
+    data = pd.read_csv("diamonds.csv")
     st.dataframe(data)
     liste_col_x = data.columns.tolist()
     liste_col_y = data.columns.tolist() 
 if data_choice == "car_crashes.csv":
-    data = pd.read_csv(r"C:\Users\KazKami\Documents\BDD exo Seaborn\car_crashes.csv")
+    data = pd.read_csv("car_crashes.csv")
     st.dataframe(data)
     liste_col_x = data.columns.tolist()
     liste_col_y = data.columns.tolist() 
